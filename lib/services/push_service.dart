0ae307e1985730ad9c53f2be3974aa7eba17d94c
tomh240106-star <tomh240106@gmail.com>
2026-09-13 16:52:25 +0200

    Create push_service.dart

---
+++ lib/services/push_service.dart
@@ -0,0 +1,11 @@
+class PushService {
+  PushService._();
+
+  static Future<void> initialize() async {
+    // Push-Benachrichtigungen werden später hier initialisiert.
+    //
+    // Diese Version ist absichtlich ohne Firebase-Abhängigkeit,
+    // damit die App zunächst erfolgreich gebaut und getestet
+    // werden kann.
+  }
+}
